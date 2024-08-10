bcb1f5f4db94fba08afd5670ad67faeea33872ff
Axel <axelfernandezcurros@gmail.com>
2024-08-10 22:41:11 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,18 @@
-import board
-import time
+import asyncio
+
+import board, time, sys, supervisor
+
 import busio
-import usb_midi
+import synthio
+import audiomixer
+
+import displayio
+import terminalio
+
+
+# from adafruit_display_text import label
+# from adafruit_displayio_layout.layouts.grid_layout import GridLayout
+# from adafruit_display_shapes.circle import Circle
 
 
 from music_midi.music_functions import *
@@ -14,7 +25,6 @@ def main(midi):
     
     set_volume(midi, 127)
 
-    
     # Example usage:
     change_instrument(midi, "Grand Piano")
     play_note(midi, 60)  # Middle C
@@ -30,8 +40,8 @@ def main(midi):
     # Uncomment the following line to run the full test
     # test_midi_synth(midi)
 
+
 if __name__ == '__main__':
-    print("Midi test")
     uart = busio.UART(tx=board.D2, baudrate=31250, bits=8, parity=None, stop=1)
     midi = adafruit_midi.MIDI(midi_out=uart, out_channel=0)
     main(midi)
